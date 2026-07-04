2d80d7dec97bf3195f345bf8cf18270d74d3a5ac
sobhan4752 <sobhan.sh89@gmail.com>
2026-07-05 02:47:45 +0330

    Delete footer.php

--- footer.php
+++
@@ -1,8 +0,0 @@
-        </main>
-    </div>
-
-    <div id="toast-container" class="fixed bottom-4 left-4 z-[100] flex flex-col gap-2 max-w-sm w-full pointer-events-none"></div>
-
-    <script src="<?= ASSETS_URL ?>js/app.js?v=<?= ASSETS_VERSION ?>"></script>
-</body>
-</html>
